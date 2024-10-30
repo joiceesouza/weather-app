8ae5b853caac0b27a695ded5e09a161da0680380
joicee.souza <joice_fsouza@hotmail.com>
2024-10-30 17:52:25 -0300

    feat(conclusao): conclui o projeto weather-app

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = "1e7d4e2f4525f9272298d01c7b1a2faf";
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {})
+  }
+}
